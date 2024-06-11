2538debb22ea9ab47a459875729deb0ee65c1510
will <willdomine1@yahoo.com>
2024-06-11 15:51:50 -0500

    Portfolio Creation
    Create nextjs react tailwind project
    npm run dev

---
+++ main/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1 className="relative bg-green-500">hello Portfolio</h1>
+      </div>
+    </main>
+  );
+}
